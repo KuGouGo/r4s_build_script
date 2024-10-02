03e825848c876c30940a28333f5dd73d62cb28d4
kugougou <1475913017@qq.com>
2024-10-02 12:59:12 +0800

    diy

--- openwrt/scripts/02-prepare_package.sh
+++ openwrt/scripts/02-prepare_package.sh
@@ -181,3 +181,38 @@ git clone https://$github/sbwml/package_kernel_tcp-brutal package/kernel/tcp-bru
 
 # watchcat - clean config
 true > feeds/packages/utils/watchcat/files/watchcat.config
+
+# add mihomo
+rm -rf package/new/helloworld/luci-app-mihomo
+rm -rf package/new/helloworld/mihomo
+git clone https://$github/pmkol/openwrt-mihomo package/new/openwrt-mihomo
+if [ "$MINIMAL_BUILD" = "y" ]; then
+    if curl -s "https://$mirror/openwrt/23-config-minimal-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
+        mkdir -p files/etc/mihomo/run/ui
+        curl -Lso files/etc/mihomo/run/GeoSite.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat
+        curl -Lso files/etc/mihomo/run/GeoIP.dat https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat
+        curl -Lso files/etc/mihomo/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
+        curl -Lso files/etc/mihomo/run/ASN.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
+        curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+        tar zxf metacubexd-gh-pages.tar.gz
+        rm metacubexd-gh-pages.tar.gz
+        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+    fi
+else
+    if curl -s "https://$mirror/openwrt/23-config-common" | grep -q "^CONFIG_PACKAGE_luci-app-mihomo=y"; then
+        mkdir -p files/etc/mihomo/run/ui
+        curl -Lso files/etc/mihomo/run/geoip.metadb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.metadb
+        curl -Lso files/etc/mihomo/run/ASN.mmdb https://$github/MetaCubeX/meta-rules-dat/releases/download/latest/GeoLite2-ASN.mmdb
+        curl -Lso metacubexd-gh-pages.tar.gz https://$github/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.tar.gz
+        tar zxf metacubexd-gh-pages.tar.gz
+        rm metacubexd-gh-pages.tar.gz
+        mv metacubexd-gh-pages files/etc/mihomo/run/ui/metacubexd
+    fi
+fi
+
+# change geodata
+rm -rf package/new/helloworld/v2ray-geodata
+git clone https://$github/sbwml/v2ray-geodata package/new/helloworld/v2ray-geodata
+sed -i 's#Loyalsoldier/geoip/releases/latest/download/geoip-only-cn-private.dat#MetaCubeX/meta-rules-dat/releases/download/latest/geoip-lite.dat#g; s#Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat#MetaCubeX/meta-rules-dat/releases/download/latest/geosite.dat#g' package/new/helloworld/v2ray-geodata/Makefile
+sed -i '/geoip_api/s#Loyalsoldier/v2ray-rules-dat#pmkol/geodata-lite#' package/new/helloworld/luci-app-passwall/root/usr/share/passwall/rule_update.lua
+sed -i '/geosite_api/s#Loyalsoldier/v2ray-rules-dat#MetaCubeX/meta-rules-dat#' package/new/helloworld/luci-app-passwall/root/usr/share/passwall/rule_update.lua

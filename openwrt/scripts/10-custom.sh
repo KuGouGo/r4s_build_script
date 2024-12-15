0550ef88c61bcf57b21d85d5bd83f87e91fa1e68
KuGouGo <62388728+KuGouGo@users.noreply.github.com>
2024-12-16 04:30:51 +0800

    Update 10-custom.sh

--- openwrt/scripts/10-custom.sh
+++ openwrt/scripts/10-custom.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 # 自定义脚本
-# dae
-git clone https://github.com/QiuSimons/luci-app-daed package/new/dae
-mkdir -p Package/libcron && wget -O Package/libcron/Makefile https://raw.githubusercontent.com/immortalwrt/packages/refs/heads/master/libs/libcron/Makefile
